792b03a4b7b7b9554572734794cf9ecd0dbdce07
Ricardo Arana Reyes <ricardo.gabriel.arana.reyes.guerrero@everis.com>
2021-06-22 11:31:52 -0500

    newmocks

--- routes/migracion-post-pre.js
+++ routes/migracion-post-pre.js
@@ -22,9 +22,16 @@ router.post('/setreglaorden', (req, res) => {
     
 });
 
-router.get('/clarosites/ofertas/consultarofertasdisponibles', (req, res) => {
+router.post('/clarosites/ofertas/consultarofertasdisponibles', (req, res) => {
     
     let obj = JSON.parse(fs.readFileSync('mocks/consultarofertasdisponibles/migracion-post-pre.json', 'utf8'));
+    res.json(obj);
+});
+
+
+router.get('/clarosites/detalleplan/consultar/:plan', (req, res) => {
+    
+    let obj = JSON.parse(fs.readFileSync('mocks/detalle-plan/all.json', 'utf8'));
     res.json(obj);
 })
 
@@ -40,4 +47,10 @@ router.post('/clarosites/ordenes/procesarorden', (req, res) => {
     let obj = JSON.parse(fs.readFileSync('mocks/procesaroden/all.json', 'utf8'));
     res.json(obj);
 })
+
+router.get('/clarosites/beneficios/consultar/:plan', (req, res) => {
+    
+    let obj = JSON.parse(fs.readFileSync('mocks/beneficios/all.json', 'utf8'));
+    res.json(obj);
+})  
 module.exports = router;
